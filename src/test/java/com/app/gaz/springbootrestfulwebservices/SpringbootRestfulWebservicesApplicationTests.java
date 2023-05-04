717dd2c86b25adcd61da2d0daea1ef3d7f532850
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-05-04 10:35:21 -0600

    first commit

---
+++ src/test/java/com/app/gaz/springbootrestfulwebservices/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gaz.springbootrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
