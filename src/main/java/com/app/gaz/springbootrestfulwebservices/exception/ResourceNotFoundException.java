491577668e104e8d12df8f695ebcb2319f81ec02
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-05-13 14:42:10 -0600

    1º uso de Excepciones de SB

---
+++ src/main/java/com/app/gaz/springbootrestfulwebservices/exception/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package com.app.gaz.springbootrestfulwebservices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String resourceName;
+    private String fielName;
+    private Long fieldValue;
+
+    public ResourceNotFoundException(String resourceName,
+                                     String fielName,
+                                     Long fieldValue){
+        super(String.format("$s Not Found !With: %s : %s", resourceName,fielName,fieldValue));
+        this.resourceName=resourceName;
+        this.fielName=fielName;
+        this.fieldValue=fieldValue;
+    }
+
+
+
+}
--- src/main/java/com/app/gaz/springbootrestfulwebservices/service/impl/UserServiceImpl.java
+++ src/main/java/com/app/gaz/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.app.gaz.springbootrestfulwebservices.service.impl;
 
 import com.app.gaz.springbootrestfulwebservices.dto.UserDto;
 import com.app.gaz.springbootrestfulwebservices.entity.User;
+import com.app.gaz.springbootrestfulwebservices.exception.ResourceNotFoundException;
 import com.app.gaz.springbootrestfulwebservices.mapper.AutoUserMapper;
 import com.app.gaz.springbootrestfulwebservices.mapper.UserMapper;
 import com.app.gaz.springbootrestfulwebservices.repository.UserRepository;
@@ -38,9 +39,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUserById(Long userId) {
-        Optional<User> optionalUser = userRepository.findById(userId);
-      // User user = optionalUser.get(); ?
-        return AutoUserMapper.MAPPER.mapToUserDto(optionalUser.get());
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", userId)
+        );
+
+        return AutoUserMapper.MAPPER.mapToUserDto(user);
     }
 
     @Override
@@ -60,7 +63,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        User exstingUser = userRepository.findById(userDto.getId()).get();
+        User exstingUser = userRepository.findById(userDto.getId()).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", userDto.getId())
+        );
+
         BeanUtils.copyProperties(userDto, exstingUser);
         User updateUser = userRepository.save(exstingUser);
         return AutoUserMapper.MAPPER.mapToUserDto(updateUser);
@@ -68,6 +74,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser(Long userId) {
+        User exstingUser = userRepository.findById(userId).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", userId)
+        );
+
         userRepository.deleteById(userId);
     }
 }
